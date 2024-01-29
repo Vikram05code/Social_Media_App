7d30350b57a1403b437efcf1aee311eebadd8e2c
Vikram <vikky5vikram@gmail.com>
2024-01-29 14:35:51 +0530

    Implemented CORS Functionlities

--- src/main/java/com/vikram/config/AppConfig.java
+++ src/main/java/com/vikram/config/AppConfig.java
@@ -1,6 +1,10 @@
 package com.vikram.config;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,6 +13,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Configuration
 @EnableWebSecurity
@@ -22,11 +31,36 @@ public class AppConfig {
 		     .authorizeHttpRequests(Authorize -> Authorize.requestMatchers("/api/**")
 				.authenticated().anyRequest().permitAll())
 		     .addFilterBefore(new jwtValidator(), BasicAuthenticationFilter.class)
-		        .csrf(csrf -> csrf.disable());
+		        .csrf(csrf -> csrf.disable())
+		        .cors(cors ->cors.configurationSource(corsConfigurationSource()));
 		 
 		return http.build();
 	}
 	
+	private CorsConfigurationSource corsConfigurationSource() {
+	
+		return new CorsConfigurationSource() {
+
+			@Override
+			public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+				
+				CorsConfiguration cfg = new CorsConfiguration();
+				
+				cfg.setAllowedOrigins(Arrays.asList(
+						"http://localhost:3000"));
+				cfg.setAllowedMethods(Collections.singletonList("*"));
+				cfg.setAllowCredentials(true);
+				cfg.setAllowedHeaders(Collections.singletonList("*"));
+				cfg.setExposedHeaders(Arrays.asList(
+						"Authorization"));
+				cfg.setMaxAge(3600L);
+				
+				return cfg;
+			}
+			
+		};
+	}
+
 	@Bean
 	PasswordEncoder passwordEncoder() {
 		
---
+++ src/main/java/com/vikram/exception/GlobalExceptions.java
@@ -0,0 +1,5 @@
+package com.vikram.exception;
+
+public class GlobalExceptions {
+
+}
